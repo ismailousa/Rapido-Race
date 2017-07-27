0002de827c44ad83248df76b8a1e607cf7ca31b1
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-07-27 09:46:12 +0300

    Track and Car movement added

---
+++ Assets/Scripts/carController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+    public float carSpeed;
+    Vector3 position;
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        transform.position = position;
+	}
+}
