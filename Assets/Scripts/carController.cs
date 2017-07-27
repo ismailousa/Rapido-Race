143d9ebdeb09075fc6cdef6371484bf1fc383428
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-07-27 11:59:07 +0300

    EnemyCar destroyer and collision added

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -19,4 +19,10 @@ public class carController : MonoBehaviour {
         position.x = Mathf.Clamp(position.x, -maxPos, maxPos);
         transform.position = position;
 	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy Car")
+            Destroy(gameObject);
+    }
 }
