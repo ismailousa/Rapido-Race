cdadea35416fcbb01726882acb22b96e497009da
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-07-27 11:29:10 +0300

    Enemy cars added

---
+++ Assets/Scripts/carSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carSpawner : MonoBehaviour {
+
+    public GameObject car;
+    public float maxPos = 2.3f;
+    public float delayTimer = 0.5f;
+    float timer;
+	// Use this for initialization
+	void Start () {
+        timer = delayTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            Vector3 carPos = new Vector3(Random.Range(-maxPos, maxPos), transform.position.y, transform.position.z);
+            Instantiate(car, carPos, transform.rotation);
+            timer = delayTimer;
+        }        
+    }
+}
